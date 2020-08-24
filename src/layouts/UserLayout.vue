22af57171185c5db142c5e6d47588bff761cb56f
Kim <7533524@qq.com>
2020-08-24 10:49:44 +0800

    fix

---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+ <router-link to="/">
+  <div class="red" to="/">首页</div>  
+  </router-link>
+ <div>登陆页面</div>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style scoped lang='less'>
+</style>

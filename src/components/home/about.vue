c3df24677324d6ea902231ae48999a99106a278a
Kim <7533524@qq.com>
2020-08-24 10:44:46 +0800

    fix

--- src/components/home/about.vue
+++ src/components/home/about.vue
@@ -1,20 +1,24 @@
 <template>
   <Card class="mt10 w100p aaaa">
     <p slot="title" class="text-left">关于</p>
-    <Badge color="cyan" text="我要反馈" />
-    <br />
-    <Badge color="cyan" text="app下载" />
-    <br />
-    <Badge color="cyan" text="交流群:904860781" />
-    <br />
-    <Badge color="cyan" text="商务合作" />
-    <br />
-    <Badge color="cyan" text="赞助本站" />
-    <br />
+    <p class="text-left mt5">
+      <Badge color="cyan" text="我要反馈" />
+    </p>
+    <p class="text-left mt5">
+      <Badge color="cyan" text="app下载" />
+    </p>
+    <p class="text-left mt5">
+      <Badge color="cyan" text="交流群:904860781" />
+    </p>
+    <p class="text-left mt5">
+      <Badge color="cyan" text="商务合作" />
+    </p>
+    <p class="text-left mt5">
+      <Badge color="cyan" text="赞助本站" />
+    </p>
   </Card>
 </template>
 <script>
-
 export default {
   data() {
     return {};
@@ -24,5 +28,4 @@ export default {
 
 
 <style scoped lang='less'>
-
 </style>

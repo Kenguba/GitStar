c6290e9db3f456f14b99700b84e3ce01dec1da5f
Kim <7533524@qq.com>
2020-08-24 20:38:51 +0800

    fix

--- src/components/home/about.vue
+++ src/components/home/about.vue
@@ -1,19 +1,19 @@
 <template>
   <Card class="mt10 w100p ">
     <p slot="title" class="text-left">关于</p>
-    <p class="text-left mt5">
+    <p class="text-left ">
       <Badge color="cyan" text="我要反馈" />
     </p>
-    <p class="text-left mt5">
+    <p class="text-left mt10">
       <Badge color="cyan" text="app下载" />
     </p>
-    <p class="text-left mt5">
+    <p class="text-left mt10">
       <Badge color="cyan" text="交流群:904860781" />
     </p>
-    <p class="text-left mt5">
+    <p class="text-left mt10">
       <Badge color="cyan" text="商务合作" />
     </p>
-    <p class="text-left mt5">
+    <p class="text-left mt10">
       <Badge color="cyan" text="赞助本站" />
     </p>
   </Card>

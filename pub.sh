9546937bd4df89b52c0d3ac2fef5199bb8d40a44
Kim <7533524@qq.com>
2020-08-25 15:51:45 +0800

    fix

--- pub.sh
+++ pub.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kill -9 $(lsof -i:7002 -t)
 cd /home/admin/app && ls | egrep  -v "^(deep|zbt|aliyun|Learn)[^.]*$" | xargs rm -rf
-sudo nginx -s reload && systemctl restart nginx && systemctl enable nginx
+sudo systemctl restart nginx && systemctl enable nginx
